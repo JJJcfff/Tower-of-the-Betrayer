096ab5f3ad97e2e5df5f555865a1a3a8c0214ac1
Elaine Zhao <dzhao24@jhu.edu>
2025-02-22 20:02:33 -0500

    fixed win/lose screen

--- Tower of the Betrayer/Assets/Scripts/WavesGameMode.cs
+++ Tower of the Betrayer/Assets/Scripts/WavesGameMode.cs
@@ -1,32 +1,39 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class WavesGameMode : MonoBehaviour
 {
     [SerializeField] private PlayerHealth playerLife;
-    
+
     void Start()
     {
-        playerLife.onDeath.AddListener(OnPlayerDeath);
         EnemyManager.instance.onChanged.AddListener(CheckWinCondition);
         WaveManager.instance.onChanged.AddListener(CheckWinCondition);
     }
-    
+
+    void Update()
+    {
+        if (playerLife != null)
+        {
+            if (playerLife.currentHealth <= 0)
+            {
+                Debug.Log("Player died. Loading LoseScreen ...");
+                SceneManager.LoadScene("LoseScreen");
+            }
+        }
+        else
+        {
+            Debug.Log("Player object is null. Loading LoseScreen.");
+            SceneManager.LoadScene("LoseScreen");
+        }
+    }
+
     void CheckWinCondition()
     {
         if (EnemyManager.instance.enemies.Count <= 0 && WaveManager.instance.waves.Count <= 0)
         {
+            Debug.Log("Player win. Loading WinScreen ...");
             SceneManager.LoadScene("WinScreen");
         }
-    }
-    
-
-    void OnPlayerDeath()
-    {
-        SceneManager.LoadScene("LoseScreen");
     }
 }
