f0bac7add626a9181f4b6bbd4af3396ccbf53503
JJJcfff <jjjcff@outlook.com>
2025-03-21 14:56:44 -0400

    added main scene

---
+++ Tower of the Betrayer/Assets/Scripts/MainSceneManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class MainSceneManager : MonoBehaviour
+{
+    public Button startGameButton;
+    
+    private void Start()
+    {
+        Debug.Log("MainSceneManager is Loaded!"); // Debug message
+        if (startGameButton != null)
+        {
+            Debug.Log("StartGameButton is assigned.");
+            startGameButton.onClick.AddListener(() => StartGame());
+        }
+        else
+        {
+            Debug.LogError("StartGameButton is NOT assigned in the Inspector!");
+        }
+    }
+    
+    private void OnButtonClicked()
+    {
+        Debug.Log("Button Click Registered! Calling StartGame()");
+        StartGame();
+    }
+
+    public void StartGame()
+    {
+        Debug.Log("StartGame() Called! Attempting to Load Home Scene...");
+        SceneManager.LoadScene("Home");
+    }
+
+    
+    
+}
